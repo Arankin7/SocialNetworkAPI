b87d68d709193e1bd8e3fce746249a5cc3ca5624
AnthonyR <arankin789@gmail.com>
2022-06-29 14:08:52 -0400

    setup thought/user models"

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,62 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
 // create Reaction Schema
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: 'A username is required to post a reaction'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)            
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
 
 // create the Thought schema
 const ThoughtSchema = new Schema(
-    {}
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJson: {
+            getters: true
+        }
+    }
 );
 
-// create virtual to return Reactions
+// create virtual to return the length of the thoughts reactions array
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 // create the Thought model
 const Thought = model('Thought', ThoughtSchema);
--- models/User.js
+++ models/User.js
@@ -2,10 +2,41 @@ const { Schema, model } = require('mongoose');
 
 // create the schema for the User
 const UserSchema = new Schema (
-    {}
+    {
+        username: {
+            type: String, 
+            unique: true,
+            required: 'A username is required.',
+            trim: true
+        },
+        email: {
+            type: String,
+            required: 'A valid email is required.',
+            unique: true,
+            match: /.+\@.+\..+/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            // not sure what is needed here yet | Array of _id values referencing the User model(self-reference) 
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
 );
 
-// use a virtual to get all thoughts
+// use a virtual to retrieve the length of the user's friends 
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+})
 
 // create the User model
 const User = model('User', UserSchema);
