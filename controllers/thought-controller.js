9b221396fd267da44767654a09ed444a73fac6b7
AnthonyR <arankin789@gmail.com>
2022-06-29 14:44:34 -0400

    made skeleton for controllers|setup index file for routes

---
+++ controllers/thought-controller.js
@@ -0,0 +1,27 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+
+    // add thought
+    addThought({ params, body }, res) {
+        
+    },
+
+    // add a reaction
+    addReaction({ params, body }, res) {
+
+    },
+
+    // remove reaction
+    removeReaction({ params }, res){
+
+    },
+
+    // remove a thought
+    removeThought({ params }, res){
+
+    }
+};
+
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,30 @@
+const { User } = require('../models');
+
+const userController = {
+    // get all users
+    getAllUsers(req, res) {
+
+    },
+
+    // get one user by ID
+    getUserById({params}, res){
+
+    },
+
+    // create User
+    createUser({body}, res) {
+
+    },
+
+    // update User
+    updateUser({params, body}, res) {
+
+    },
+
+    // delete User
+    deleteUser({params}, res){
+
+    }
+};
+
+module.exports = userController;
